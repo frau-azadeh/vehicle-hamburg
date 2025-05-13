5677b9597774230137eadd1373c78d49f8ea7791
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-05-13 18:33:48 +0330

    chore: add barrel file for vehicles feature exports

---
+++ src/features/vehicles/index.tsx
@@ -0,0 +1,4 @@
+export * from './types';
+export { default as useVehicles } from '../hooks/useVehicles';
+export { default as VehicleList } from './components/VehicleList';
+export { default as VehiclePanel } from './components/VehiclePanel';
