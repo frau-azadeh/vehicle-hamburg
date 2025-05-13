cfb1f91fdeb461a0beb0f07b72f244ecb77ae213
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-05-13 17:19:27 +0330

    feat: define custom Leaflet marker icons for default and selected states
    Added colored marker icons with custom anchor and shadow settings to distinguish selected vehicles visually on the map.

---
+++ src/features/vehicles/map/mapIcons.ts
@@ -0,0 +1,19 @@
+import L from 'leaflet';
+
+export const defaultIcon = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+export const selectedIcon = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
