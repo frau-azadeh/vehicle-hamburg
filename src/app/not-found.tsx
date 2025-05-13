00f68f924cf0194c9807934263a7a974bd843303
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-05-13 16:24:11 +0330

    feat: implement custom 404 page with accessible back button
    Designed a user-friendly 404 page using a centered layout, icon-enhanced outline button, and keyboard-accessible navigation for improved UX.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { ArrowLeft } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import Button from '@/features/vehicles/components/ui/Button';
+
+ function NotFound() {
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen px-4 text-center text-text">
+      <h1 className="mb-4 text-6xl font-bold text-primary">404</h1>
+      <h2 className="mb-2 text-2xl font-semibold">Oops! Not Found</h2>
+      <p className="mb-6 text-muted">
+        We couldn’t find what you were looking for.
+      </p>
+
+      <Button
+        variant="outline"
+        onClick={() => router.back()}
+        className="flex items-center gap-2 text-sm"
+        aria-label="Go back to previous page"
+      >
+        <ArrowLeft size={16} />
+        Go Back
+      </Button>
+    </div>
+  );
+}
+export default NotFound
