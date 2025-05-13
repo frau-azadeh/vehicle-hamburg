6086ec917f5a6535e0c4aaae1714190dac42ae4f
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-05-13 16:22:23 +0330

    feat: create reusable Button component with variant support
    Implemented a customizable Button component with support for 'default' and 'outline' variants using Tailwind and clsx for consistent UI styling.

---
+++ src/features/vehicles/components/ui/Button.tsx
@@ -0,0 +1,25 @@
+import { clsx } from 'clsx';
+import { ButtonHTMLAttributes } from 'react';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: 'default' | 'outline';
+};
+const Button =({
+  variant = 'default',
+  className,
+  ...props
+}: ButtonProps) =>{
+  const baseClass = clsx(
+    'text-sm px-4 py-2 rounded-soft font-medium transition duration-150',
+    {
+      'bg-primary text-white hover:bg-primary/90 shadow': variant === 'default',
+
+      'bg-outline text-white border border-border hover:bg-outline/80 shadow-sm':
+        variant === 'outline',
+    },
+    className
+  );
+
+  return <button className={baseClass} {...props} />;
+}
+export default Button
