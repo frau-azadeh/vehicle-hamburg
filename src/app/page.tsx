1308414dc980dc332edb57749a71918ece1762c2
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-05-14 09:30:35 +0330

    chore: run prettier autofix to enforce code style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
- function Home() {
+function Home() {
   const router = useRouter();
 
   useEffect(() => {
@@ -35,4 +35,4 @@ import { useRouter } from 'next/navigation';
     </div>
   );
 }
-export default Home
+export default Home;
